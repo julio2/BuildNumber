4b220b7a6afc083f7613bd1946e7e47ede326f7e
Julio Gambandé <gambande.julio@gmail.com>
2020-04-30 11:42:06 -0300

    Version mejorada del prorama original - palabra clave: BUILDNUMBER

--- README.md
+++
@@ -1,2 +0,0 @@
-# BuildNumber
-Actualizacion automatica del numero de compilacion para incluir en los proyectos, (Es necesario indicar al IDE que debe ejecutar este programa antes de compilar)
---
+++ version.h
@@ -0,0 +1,33 @@
+/* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+#ifndef XC_VERSION_H
+#define	XC_VERSION_H
+
+#include <stdlib.h>
+#include <string.h>
+#include "GenericTypeDefs.h"
+
+
+typedef struct _UINT24{
+       UINT8 H;
+       UINT8 LH;
+       UINT8 LL;
+   }UINT24; 
+   
+#define VERSION "v1.08"
+#define MODELO  "C226T"   
+#define BUILDNUMBER 108
+
+void empaquetarVersion(char* buffer);
+
+#endif	/* XC_VERSION_H */
+
+
+
+
+
